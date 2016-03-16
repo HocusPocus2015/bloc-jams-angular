4db4302e4d5d3867ce0aeea094542bdef9a25e27
Alice Suckling <alicesuckling@live.co.uk>
2016-03-16 18:21:32 +0000

    album controller and template change to reflect binding data

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,8 @@
+(function() {
+  function AlbumCtrl() {
+    this.albumData.push(angular.copy(albumPicasso));
+  }
+  angular
+    .module('blocJams')
+    .controller('AlbumCtrl', AlbumCtrl);
+})();
