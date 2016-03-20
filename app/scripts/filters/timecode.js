e2608f038a62feb26e57b55f82e52c32e584f7bf
Alice Suckling <alicesuckling@live.co.uk>
2016-03-20 12:17:51 +0000

    WIP

---
+++ app/scripts/filters/timecode.js
@@ -0,0 +1,21 @@
+(function() {
+  function timecode() {
+    return functon(seconds) {
+      var seconds = NUmber.parseFloat(seconds);
+      var wholeSeconds = Math.floor(seconds);
+      var minutes = Math.floor(wholeSeconds / 60);
+      var remainingSeconds = wholeSeconds % 60;
+      
+      var output = minutes + ':';
+      if (remainingSeconds < 10) {
+        output += '0';
+      }
+      output += remainingSeconds;
+      return output;
+    };
+  }
+  
+  angular
+    .module('blocJams')
+    .filter('timecode', timecode);
+})();
